d5f1145397719da1d03f120423bdfd65e7e00edd
Claudisu Mainja <claudius.mainja@gmail.com>
2026-04-26 01:39:13 +0200

    Revert "Update navbar button to 'View Our Activity Prices' with arrow icon"
    This reverts commit c062cc57208d725833a20a2a8c8c528d019b6982.

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,28 +1,19 @@
 import { Outlet, useLocation } from 'react-router-dom'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import Header from './Header'
 import Footer from './Footer'
 
 export default function Layout() {
   const location = useLocation()
-  const [scrollY, setScrollY] = useState(0)
   
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [location])
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollY(window.scrollY)
-    }
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
-
   return (
-    <div className="min-h-screen flex flex-col" style={{ transform: `translateY(${scrollY * 0.1}px)` }}>
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1 pt-0">
+      <main className="flex-1">
         <Outlet />
       </main>
       <Footer />
